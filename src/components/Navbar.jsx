ca27a8c0c78e28258da2a3de6fcb03e8ff23a8a9
Andre Barros <andrebarros7201@gmail.com>
2024-06-21 13:15:21 +0100

    change navbar links to buttons

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,16 @@
 import { Link } from "react-router-dom";
+import "../styles/navbar.css";
 
 function Navbar() {
   return (
     <header>
       <ul>
-        <Link to="/">Home</Link>
-        <Link to="/shop">Shop</Link>
+        <button>
+          <Link to="/">Home</Link>
+        </button>
+        <button>
+          <Link to="/shop">Shop</Link>
+        </button>
       </ul>
     </header>
   );
