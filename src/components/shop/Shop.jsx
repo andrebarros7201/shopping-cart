9b0d23108fff024295621e9e9ba4bb10c39fece0
Andre Barros <andrebarros7201@gmail.com>
2024-06-21 18:57:18 +0100

    filter data based on category

--- src/components/shop/Shop.jsx
+++ src/components/shop/Shop.jsx
@@ -10,6 +10,7 @@ function Shop() {
   const [data, setData] = useState([]);
   const [categories, setCategories] = useState([]);
   const [shoppingCart, setShoppingCart] = useState([]);
+  const [filterCategory, setFilterCategory] = useState(null);
 
   const totalCart = parseFloat(
     shoppingCart
@@ -26,12 +27,20 @@ function Shop() {
   }, []);
 
   useEffect(() => {
-    fetch("https://fakestoreapi.com/products")
-      .then((res) => res.json())
-      .then((json) => setData(json))
-      .catch((error) => setError(error))
-      .finally(setLoading(false));
-  }, []);
+    if (!filterCategory) {
+      fetch("https://fakestoreapi.com/products")
+        .then((res) => res.json())
+        .then((json) => setData(json))
+        .catch((error) => setError(error))
+        .finally(setLoading(false));
+    } else {
+      fetch(`https://fakestoreapi.com/products/category/${filterCategory}`)
+        .then((res) => res.json())
+        .then((json) => setData(json))
+        .catch((error) => setError(error))
+        .finally(setLoading(false));
+    }
+  }, [filterCategory]);
 
   const addItemToCart = (item, quantity) => {
     if (shoppingCart.filter((x) => x.name === item.title).length === 0) {
@@ -56,6 +65,10 @@ function Shop() {
     setShoppingCart(shoppingCart.filter((x) => x.name !== item.name));
   };
 
+  const handleChangeCategory = (category) => {
+    setFilterCategory(category);
+  };
+
   if (loading) return <h1>Loading...</h1>;
   if (error) return <h1>Something went wrong...</h1>;
   return (
@@ -63,9 +76,14 @@ function Shop() {
       <main>
         <div className="categories">
           <ul>
+            <li key={"all"}>
+              <button onClick={() => handleChangeCategory(null)}>ALL</button>
+            </li>
             {categories.map((category) => (
               <li key={category}>
-                <button>{String(category).toUpperCase()}</button>
+                <button onClick={() => handleChangeCategory(category)}>
+                  {String(category).toUpperCase()}
+                </button>
               </li>
             ))}
           </ul>
